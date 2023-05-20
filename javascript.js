68828c3f3837fd3ac018e2f7b513e6ed7fa676ad
Derek Nyssen <nyssen@gmail.com>
2023-05-20 15:27:19 -0700

    add grid using javascript methods

--- javascript.js
+++ javascript.js
@@ -1,8 +1,18 @@
 const container = document.querySelector(".container");
 
 for (let i = 0; i < 16; i++) {
-    const newDiv = document.createElement('div');
-    container.appendChild(newDiv);
-    newDiv.className = "row";
+    const row = document.createElement('div');
+    container.appendChild(row);
+    row.className = "row";
+    for (let i = 0; i < 16; i++) {
+        const box = document.createElement('div');
+        row.appendChild(box);
+        box.className = "box";
+    }
 }
+
+
+
+
+
 
