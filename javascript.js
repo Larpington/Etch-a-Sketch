0acc42c9f023d2e9ed1846f54c3c1e478e9c762c
Derek Nyssen <nyssen@gmail.com>
2023-05-22 14:33:01 -0700

    update changeGridSize to delete and remake grid

--- javascript.js
+++ javascript.js
@@ -22,14 +22,34 @@ boxes.forEach((box) => {
 });
 
 function changeGridSize(){
-    let numberOfBoxes = prompt("How many boxes would you like per row? (100 max)")
+    let numberOfBoxes = prompt("How many boxes would you like per row? (100 max)");
     if (numberOfBoxes > 100) {
-        const errorMessage = document.querySelector(".error-box")
-        errorMessage.textContent = "The maximum row size is 100, please try again."
+        const errorMessage = document.querySelector(".ui-box");
+        errorMessage.textContent = "The maximum row size is 100, please try again.";
     } else if (isNaN(numberOfBoxes)) {
-        const errorMessage = document.querySelector(".error-box")
-        errorMessage.textContent = "Please only input a number, up to 100."
+        const errorMessage = document.querySelector(".ui-box");
+        errorMessage.textContent = "Please only input a number, up to 100.";
+    } else {
+        const boxes = document.querySelectorAll(".box")
+        boxes.forEach(box => {
+            box.remove();
+        });
+        const rows = document.querySelectorAll(".row")
+        rows.forEach(row => {
+            row.remove();
+        });
+        for (let i = 0; i < numberOfBoxes; i++) {
+            const row = document.createElement('div');
+            container.appendChild(row);
+            row.className = "row";
+            for (let i = 0; i < numberOfBoxes; i++) {
+                const box = document.createElement('div');
+                row.appendChild(box);
+                box.className = "box";
+        }
+        }
     }
+
 }
 
 
